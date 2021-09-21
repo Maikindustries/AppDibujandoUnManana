461c1225f0d515ccb3660936a7f51b7bce5a90a7
Maikindustries <mikeperezlopez15@hotmail.com>
2021-09-20 23:24:21 -0500

    ui changes

--- app/src/main/java/mx/itesm/appdibujandounmanana/ui/login/LoginActivity.kt
+++ app/src/main/java/mx/itesm/appdibujandounmanana/ui/login/LoginActivity.kt
@@ -3,7 +3,10 @@ package mx.itesm.appdibujandounmanana.ui.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.core.graphics.toColor
+import mx.itesm.appdibujandounmanana.R
 import mx.itesm.appdibujandounmanana.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
