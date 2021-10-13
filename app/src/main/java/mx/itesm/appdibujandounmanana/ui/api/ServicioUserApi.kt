aa240cc70d3f4367685e972f6dac84449b6f9a7a
AriadnaHCTec <65170456+AriadnaHCTec@users.noreply.github.com>
2021-10-12 23:33:55 -0500

    Rutas

--- app/src/main/java/mx/itesm/appdibujandounmanana/ui/api/ServicioUserApi.kt
+++ app/src/main/java/mx/itesm/appdibujandounmanana/ui/api/ServicioUserApi.kt
@@ -7,12 +7,28 @@ import retrofit2.http.*
 
 // https://disease.sh/v3/covid-19/hi    storical/mexico?lastdays=5
 interface ServicioUserApi {
+    //Usuario
     @POST("user/agregarUsuario")
     fun agregarUsuario(@Body userData: JsonUserData) : retrofit2.Call<String>
+    @POST("user/IniciarSesion") //User Pass
+    fun iniciarSesion(@Body userData: JsonUserData) : retrofit2.Call<String>
+    @POST("user/validarCorreo")
+    fun validarCorreo(@Body userData: JsonUserData) : retrofit2.Call<String>
+    @POST("user/updatePassword")
+    fun updatePassword(@Body userData: JsonUserData) : retrofit2.Call<String>
 
-    @POST("user/iniciarSesion")
-    fun iniciarSesion(/*@Body userData: JsonUsuario*/) : retrofit2.Call<String>
+    //Organizacion
+    @POST("user/agregarOrganizacion")
+    fun agregarOrganizacion(@Body userData: JsonUserData) : retrofit2.Call<String>
+    @POST("user/iniciarSesionOrganizacion")
+    fun iniciarSesionOrganizacion(@Body userData: JsonUserData) : retrofit2.Call<String>
 
+    //Donaciones
     @POST("user/registros")
     fun descargarDonaciones(/**/): retrofit2.Call<Donacion>
+
+    //Admin
+    @POST("user/proyectos")
+    fun proyectos(/**/): retrofit2.Call<Donacion>
+
 }
